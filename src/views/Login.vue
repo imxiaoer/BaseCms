8d402fc92fe02a1d1ca18b05efb558a2a3e4cb9e
yunfeng <281542025@qq.com>
2019-03-22 17:17:52 +0800

    日常开发

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -56,7 +56,7 @@ export default {
             localStorage.setItem('key', res.data.data.user_key)
             this.loading = false
             this.$router.replace({ path: 'console' })
-          }, error => {
+          }, () => {
             this.loading = false
           })
         }
