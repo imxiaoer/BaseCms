55a9f04937108f858f0f18188a17810bc614d9c1
yunfeng <281542025@qq.com>
2019-04-10 19:39:26 +0800

    日常开发

--- src/mock/roles.js
+++ src/mock/roles.js
@@ -2,21 +2,20 @@
  * 角色模块
  */
 
- import Mock from 'mockjs'
+import Mock from 'mockjs'
 
- const role = {
-    list: Mock.mock('http://localhost:8080/api/role/list', 'get', option => {
-        
-    }),
-    add: Mock.mock('', 'post', option => {
+const role = {
+  list: Mock.mock('http://localhost:8080/api/role/list', 'get', option => {
+  }),
+  add: Mock.mock('', 'post', option => {
 
-    }),
-    modify: Mock.mock('', 'post', option => {
+  }),
+  modify: Mock.mock('', 'post', option => {
 
-    }),
-    remove: Mock.mock('', 'post', option => {
+  }),
+  remove: Mock.mock('', 'post', option => {
 
-    })
- }
+  })
+}
 
- export default role
+export default role
